9f57a20a4eedeb38b8cb21b05c24cc472411f903
alexnitta <a.j.nitta@gmail.com>
2016-03-25 20:23:30 -0700

    Add options, remove config file, initial pass at database connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,30 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'p',
+//   database: 'chat'
+// });
+
+// connection.connect();
+
+// // will take data and save it to database (for POST requests)
+// exports.save = function() {
+
+// };
+
+// // query the database (for GET requests)
+// exports.query = function() {
+
+// };
+
+// // remove from database (do we need this?)
+// exports.remove = function() {
+
+// };
+
+// connection.query();
+
 
