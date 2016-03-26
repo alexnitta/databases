e3f2f207cdef5afe4fd742630aa91f5b158355f8
alexnitta <a.j.nitta@gmail.com>
2016-03-25 17:39:00 -0700

    Add screenshots for 2 schemas, add mySQL npm module, add schema in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,80 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Messages`;
+    
+CREATE TABLE `Messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `User_ID` INTEGER(5) NOT NULL DEFAULT 0,
+  `Room_ID` INTEGER(5) NOT NULL DEFAULT 0,
+  `Text` VARCHAR(300) NULL DEFAULT '',
+  PRIMARY KEY (`id`)
 );
+
+-- ---
+-- Table 'Rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Rooms`;
+    
+CREATE TABLE `Rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `Name` VARCHAR(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Users`;
+    
+CREATE TABLE `Users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `Name` VARCHAR(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (User_ID) REFERENCES `Users` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (Room_ID) REFERENCES `Rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Messages` (`id`,`User_ID`,`Room_ID`,`Text`) VALUES
+-- ('','','','');
+-- INSERT INTO `Rooms` (`id`,`Name`) VALUES
+-- ('','');
+-- INSERT INTO `Users` (`id`,`Name`) VALUES
+-- ('','');
+
 
 /* Create other tables and define schemas for them here! */
 
@@ -12,6 +83,6 @@ CREATE TABLE messages (
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u root -p < server/schema.sql
  *  to create the database and the tables.*/
 
