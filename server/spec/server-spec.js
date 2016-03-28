1cefa634ba1069d30885c84ccdaafa657059565d
Alex Nitta <a.j.nitta@gmail.com>
2016-03-28 08:50:26 -0700

    Fix test spec

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -67,13 +67,13 @@ describe('Persistent Node Chat Server', function() {
 
   it('Should output all messages from the DB', function(done) {
     // Let's insert a message into the db
-    var queryString = 'SELECT * FROM Messages';
-    // var queryArgs = [];
+    var queryString = 'INSERT INTO Messages(text, userid, roomname) VALUES (?, ?, ?)';
+    var queryArgs = ['Men like you can never change!', 1, 'main'];
     // TODO - The exact query string and query args to use
     // here depend on the schema you design, so I'll leave
     // them up to you. */
 
-    dbConnection.query(queryString, /*queryArgs,*/ function(err) {
+    dbConnection.query(queryString, queryArgs, function(err) {
       if (err) { throw err; }
       
       // Now query the Node chat server and see if it returns
